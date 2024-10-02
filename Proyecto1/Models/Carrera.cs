f6defdf84531521e296c8c4f7c767821ae37ed9e
camiloo1210 <camiloandresbrazalesbarrero@gmail.com>
2024-10-02 09:05:02 -0500

    Clases en c#

---
+++ Proyecto1/Models/Carrera.cs
@@ -0,0 +1,6 @@
+﻿namespace Proyecto1.Models
+{
+    public class Carrera
+    {
+    }
+}
---
+++ Proyecto1/Models/EstudianteUdla.cs
@@ -0,0 +1,12 @@
+﻿namespace Proyecto1.Models
+{
+    public class EstudianteUdla
+
+    {
+        public string IdBanner {  get; set; }
+        public string Nombre { get; set; }
+        public string Correo { get; set; }
+        public Carrera Carrera { get; set; }
+
+    }
+}
