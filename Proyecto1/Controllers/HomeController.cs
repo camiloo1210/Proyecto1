9979f63c2ef2ea78db6b718f1e4e8bd8a66e9780
camiloo1210 <camiloandresbrazalesbarrero@gmail.com>
2024-10-02 08:36:09 -0500

    uwuadas

--- Proyecto1/Controllers/HomeController.cs
+++ Proyecto1/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 
 namespace Proyecto1.Controllers
 {
+    //vbjklñ
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
